f7b5e8f5427fa8138de86134316334ccda61b030
javad9977 <javadaboosi.9977@gmail.com>
2022-05-28 20:20:24 +0430

    updated project step 3

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,61 +1,48 @@
-import React , {useState , useEffect , useCallback} from 'react'
+import React, { useCallback, useReducer } from 'react'
 
 import ProductForm from './ProductForm'
 import ProductList from './ProductList'
 import Search from './Search'
 
-function Products() {
-  const [products , setProduct] = useState([])
-  useEffect(()=>{
-    fetch('https://todo-list-83e6e-default-rtdb.firebaseio.com/products.json')
-    .then((response)=>{
-    return response.json()
-  }).then((responseData)=>{
-    const loadproducts=[]
-    for(const item in responseData){
-      loadproducts.push({
-        id:item,
-        title:responseData[item].title,
-        amount:responseData[item].amount
-        
-      })
-    }
-    setProduct(loadproducts)
-  })
-
-  },[])
-
-  
-  const addProductHandler=(item)=>{
-    fetch('https://todo-list-83e6e-default-rtdb.firebaseio.com/products.json',{
-      method:'POST',
-      body:JSON.stringify(item),
-      headers:{'Content-Type':'application/json'}
-    }).then((response)=>{
-        response.json()
-        .then((responseData)=>{
-          setProduct((previtem)=>{
-            return [
-              ...previtem , 
-              {
-                id:responseData.name,
-                ...item
-              }
-            ]
-          })
-        })
-    })
-   
+const productReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.products
+    case 'ADD':
+      return [...state, action.product]
+    default:
+      throw new Error('Error')
   }
-  const onAddproduct=useCallback((items)=>{
-    setProduct(items)
-  },[])
+}
+
+const Products = () => {
+  const [products, dispath] = useReducer(productReducer, [])
+
+  const searchProductsHandler = useCallback((items) => {
+    dispath({ type: 'SET', products: items })
+  }, [])
+
+  const addProductHandler = (item) => {
+    fetch('https://todo-list-83e6e-default-rtdb.firebaseio.com/products.json', {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: { 'Content-Type': 'application/json' },
+    }).then((response) => {
+      response.json().then((responseData) => {
+        dispath({
+          type: 'ADD',
+          product: { id: responseData.name, ...item },
+        })
+      })
+    })
+  }
+
   return (
     <div className="App">
       <ProductForm onaddedproduct={addProductHandler} />
 
       <section>
-        <Search  onloadedproductssss={onAddproduct} />
+        <Search  onloadedproductssss={searchProductsHandler} />
         <ProductList products={products} />
       </section>
     </div>

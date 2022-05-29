7138477e0a2beff5fabb38a86c45d1117bf892b0
javad9977 <javadaboosi.9977@gmail.com>
2022-05-29 17:57:31 +0430

    updated project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React  from 'react'
+import React ,{useContext} from 'react'
 import Products from './components/Products/Products'
-
+import Auth from './components/Auth'
+import { AuthContext } from './components/Context/AuthContext'
 const App = () => {
+  const authservice = useContext(AuthContext)
 
-return(
-   <Products/>
-)
+  let content = <Auth/>
+
+  if(authservice.isAuth){
+    content = <Products/>
+  }
+return content
 
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import AuthContextProvider from './components/Context/AuthContext'
 ReactDOM.render(
-    <BrowserRouter>
+<AuthContextProvider>
 <App />
-</BrowserRouter>
+</AuthContextProvider>
 , document.getElementById('root'))

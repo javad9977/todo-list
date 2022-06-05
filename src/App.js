5c0facf6ba1ebf20f310b8e66e507f1298a4d131
javad9977 <javadaboosi.9977@gmail.com>
2022-06-05 17:41:59 +0430

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React ,{useContext} from 'react'
 import Products from './components/Products/Products'
 import Auth from './components/Auth'
+import UserDarkMode from './../src/hooks/dark-mode'
 import { AuthContext } from './components/Context/AuthContext'
 const App = () => {
   const authservice = useContext(AuthContext)
-
+  const [theme , toggletheme]=UserDarkMode()
   let content = <Auth/>
 
   if(authservice.isAuth){
-    content = <Products/>
+    content = (
+      <div style={{ 
+
+        background: theme==='dark'?'#000':'#fff',
+        color: theme==='dark'?"#fff":'#212121',
+        transition:'.2s all',
+        textAlign:'center',
+        height:"100vh"
+
+      }}>
+
+        <Products/>
+
+
+        <button onClick={toggletheme}>تغییر تم</button>
+
+      </div>
+      )
   }
 return content
 
